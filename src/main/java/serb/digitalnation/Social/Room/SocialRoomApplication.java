e774676867f8210114660af39294192f9e00c7b0
Seerbbran <tonieserban@yahoo.ro>
2024-11-11 14:13:03 +0100

    first commit

---
+++ src/main/java/serb/digitalnation/Social/Room/SocialRoomApplication.java
@@ -0,0 +1,13 @@
+package serb.digitalnation.Social.Room;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialRoomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/serb/digitalnation/Social/Room/SocialRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package serb.digitalnation.Social.Room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
